f339917618970f21d0fef0f65f741dd6f37fd98f
Robert Bennett <robertbennett1998@outlook.com>
2020-05-04 15:02:18 +0100

    Check

--- cats_and_dogs.py
+++ cats_and_dogs.py
@@ -20,8 +20,16 @@ def model_exception_handler(e):
 
 model_configuration = hpo.ModelConfiguration(optimiser=cats_and_dogs_models.optimiser, layers=cats_and_dogs_models.cnn, loss_function="categorical_crossentropy", number_of_epochs=10)
 
+####################################
+# Random Search
+####################################
+strategy = hpo.strategies.random_search.RandomSearch(model_configuration, 100)
+hpo_instance = hpo.Hpo(model_configuration, construct_cats_and_dogs_data, strategy, model_exception_handler=model_exception_handler)
+
+hpo_experiment_runner.run(hpo_instance, os.path.join(os.getcwd(), "cats_and_dogs_hpo_random_search.results"))
+
 #####################################
-# Bayesian Selection - Random Forest
+# Bayesian Selection - Gaussian Process
 #####################################
 strategy = hpo.strategies.bayesian_method.BayesianMethod(model_configuration, 100, hpo.strategies.bayesian_method.GaussianProcessSurrogate())
 hpo_instance = hpo.Hpo(model_configuration, construct_cats_and_dogs_data, strategy, model_exception_handler=model_exception_handler)
@@ -29,20 +37,29 @@ hpo_instance = hpo.Hpo(model_configuration, construct_cats_and_dogs_data, strate
 hpo_experiment_runner.run(hpo_instance, os.path.join(os.getcwd(), "cats_and_dogs_hpo_bayesian_gaussian_process.results"))
 
 #####################################
-# Random Search
+# Bayesian Selection - Random Forest
 #####################################
-#strategy = hpo.strategies.random_search.RandomSearch(model_configuration, 100)
-#hpo_instance = hpo.Hpo(model_configuration, construct_cats_and_dogs_data, strategy, model_exception_handler=model_exception_handler)
+strategy = hpo.strategies.bayesian_method.BayesianMethod(model_configuration, 100, hpo.strategies.bayesian_method.RandomForestSurrogate())
+hpo_instance = hpo.Hpo(model_configuration, construct_cats_and_dogs_data, strategy, model_exception_handler=model_exception_handler)
 
-#hpo_experiment_runner.run(hpo_instance, os.path.join(os.getcwd(), "cats_and_dogs_hpo_random_search.results"))
+hpo_experiment_runner.run(hpo_instance, os.path.join(os.getcwd(), "cats_and_dogs_hpo_bayesian_random_forest.results"))
 
+########################################
+# Genetic Algorithm - Threshold Selection
 #########################################
-# Genetic Algorithm - Roulette Selection
-##########################################
-#strategy = hpo.strategies.genetic_algorithm.GeneticAlgorithm(population_size=10, max_iterations=10, chromosome_type=construct_chromosome,
-#                                           survivour_selection_stratergy="roulette")
-#strategy.mutation_strategy().mutation_probability(0.05)
-#strategy.survivour_selection_strategy().survivour_percentage(0.7)
-#hpo_instance = hpo.Hpo(model_configuration, construct_cats_and_dogs_data, strategy, model_exception_handler=model_exception_handler)
+strategy = hpo.strategies.genetic_algorithm.GeneticAlgorithm(population_size=10, max_iterations=10, chromosome_type=construct_chromosome, survivour_selection_stratergy="roulette")
+strategy.mutation_strategy().mutation_probability(0.05)
+strategy.survivour_selection_strategy().survivour_percentage(0.7)
+hpo_instance = hpo.Hpo(model_configuration, construct_cats_and_dogs_data, strategy, model_exception_handler=model_exception_handler)
 
-#hpo_experiment_runner.run(hpo_instance, os.path.join(os.getcwd(), "cats_and_dogs_hpo_genetic_algorithm_roulette.results"))
+hpo_experiment_runner.run(hpo_instance, os.path.join(os.getcwd(), "cats_and_dogs_hpo_genetic_algorithm_roulette.results"))
+
+########################################
+# Genetic Algorithm - Roulette Selection
+#########################################
+strategy = hpo.strategies.genetic_algorithm.GeneticAlgorithm(population_size=10, max_iterations=10, chromosome_type=construct_chromosome, survivour_selection_stratergy="threshold")
+strategy.mutation_strategy().mutation_probability(0.05)
+strategy.survivour_selection_strategy().threshold(0.9)
+hpo_instance = hpo.Hpo(model_configuration, construct_cats_and_dogs_data, strategy, model_exception_handler=model_exception_handler)
+
+hpo_experiment_runner.run(hpo_instance, os.path.join(os.getcwd(), "cats_and_dogs_hpo_genetic_algorithm_roulette.results"))
